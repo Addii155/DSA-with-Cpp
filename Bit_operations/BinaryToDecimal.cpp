758111d7dc2cdee6a2940368120b992bfbaa50e9
Aditya Kumar <aditysgoyal11@gmail.com>
2024-09-11 18:54:32 +0530

    bit operation

---
+++ Bit_operations/BinaryToDecimal.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int convert_BinarytoDecimal(string str)
+{
+    int result = 0;
+    int n = str.length();
+    for (int i = n - 1; i >= 0; i--)
+    {
+        char ch = str[i];
+        int num = ch - '0';
+        result = result + num * (1 << (n - i - 1));
+    }
+    return result;
+}
+int main()
+{
+    string str = "1010";
+    cout << convert_BinarytoDecimal(str);
+    return 0;
+}
