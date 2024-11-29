93240885381ceb9d55189525c14c063c86506f67
Aditya Gupta <148351333+AaDiTYAKumaR1@users.noreply.github.com>
2024-11-29 16:37:16 +0530

    heapsort

---
+++ heap/heapImplement.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class Minheap{
+    public:
+    int idx=1;
+    int arr[50];
+    int top()
+    {
+        return arr[1];
+    }
+    void push(int x){
+        arr[idx]=x;
+        int i=idx;
+        idx++;
+        while(i!=1)
+        {
+            int parent=arr[i/2];
+            if(parent>x){
+                swap(arr[i/2],arr[i]);     
+            }
+            else break;
+            i=i/2;
+        }
+    }
+    void display(){
+        for(int i=1;i<idx;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+    // void pop(){
+    //     arr[1]=arr[idx-1];
+
+    // }
+};
+
+int main(){
+    Minheap m1;
+    m1.push(10);
+    m1.push(20);
+    m1.push(30);
+    m1.push(3);
+    m1.display();
+    m1.push(50);
+    m1.push(5);
+    m1.display();
+}
