5e318ea9b2dd7e8529c8fc83adb2eec78b6aaed6
Aditya Gupta <148351333+AaDiTYAKumaR1@users.noreply.github.com>
2025-05-14 13:45:46 +0530

    Refactor permutation function for clarity and remove redundant output

--- BackTacking/AllPermutation.cpp
+++ BackTacking/AllPermutation.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 void permute(string &s,int l,int r)
 {
+    if(l==r) //base case when the left index is equal to the right index
+    {
+        cout<<s<<endl; //print the current permutation of the string
+        return;
+    }
+    // cout<<s<<endl; //print the current permutation of the string
     for(int i=l;i<=r;i++)
     {
         swap(s[l],s[i]);
         permute(s,l+1,r); //recursive call to find all the permutations of the string
-        // swap(s[l],s[i]); //backtrack to the previous state
-        cout<<s<<endl; //print the current permutation of the string
+        swap(s[l],s[i]); //backtrack to the previous state
+         //print the current permutation of the string
     }
 }
 int main()
@@ -18,3 +24,4 @@ int main()
     permute(s,0,s.size()-1); //function to find all the permutations of the string
     return 0;
 }
+//  a s ->a s 
